72794bd255fc2510a804cf0c0b2dd6ac4adac96a
marshfellow42 <leandrocsousa9@gmail.com>
2023-10-12 21:27:53 -0300

    Criação do nosso projeto no Netbeans

---
+++ App_Clima/src/main/java/br/edu/principal/App_Clima.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.principal;
+
+/**
+ *
+ * @author mello
+ */
+public class App_Clima {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
