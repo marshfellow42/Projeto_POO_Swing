37e22ea61b3c03c076d0a953fb8e65f6997f0584
marshfellow42 <leandrocsousa9@gmail.com>
2023-10-16 01:22:17 -0300

    Consertando nosso projeto
    Eu consegui consertar o nosso projeto pois eu estava trabalhando nele em uma branch passada, mas eu me esqueci de portar ele para a main, assim quase corrompendo tudo, mas eu consegui salvar o projeto e agora eu estou commitando ele de verdade aqui

---
+++ App_Clima/src/DAO/UsuarioDAO.java
@@ -0,0 +1,52 @@
+package DAO;
+
+import DTO.UsuarioDTO;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import javax.swing.JOptionPane;
+
+public class UsuarioDAO {
+
+    Connection conn;
+
+    public ResultSet autenticacaoUsuario(UsuarioDTO objusuariodto) {
+        conn = new ConecSQL().conectaBD();
+
+        try {
+            String sql = "select * from usuario where nome_usuario = ? and senha_usuario = ?";
+
+            PreparedStatement pstm = conn.prepareStatement(sql);
+            pstm.setString(1, objusuariodto.getNome_usuario());
+            pstm.setString(2, objusuariodto.getSenha_usuario());
+
+            ResultSet rs = pstm.executeQuery();
+            return rs;
+
+        } catch (SQLException error) {
+            JOptionPane.showMessageDialog(null, "UsuarioDAO (autenticação): " + error);
+            return null;
+        }
+
+    }
+
+    public void cadastrarUsuario(UsuarioDTO objusuariodto) {
+        PreparedStatement pstm;
+        String sql = "insert into usuario (nome_usuario,senha_usuario) values (?,?)";
+
+        conn = new ConecSQL().conectaBD();
+
+        try {
+            pstm = conn.prepareStatement(sql);
+            pstm.setString(1, objusuariodto.getCriar_nome_usuario());
+            pstm.setString(2, objusuariodto.getCriar_senha_usuario());
+
+            pstm.execute();
+            pstm.close();
+
+        } catch (SQLException error) {
+            JOptionPane.showMessageDialog(null, "UsuarioDAO (cadastrar)" + error);
+        }
+    }
+}
