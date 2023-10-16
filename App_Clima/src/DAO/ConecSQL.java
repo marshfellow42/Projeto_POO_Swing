3a927499cf25c80371e0352fe4087ce20962cc62
marshfellow42 <leandrocsousa9@gmail.com>
2023-10-16 00:53:52 -0300

    Adicionar o sistema de mostrar a senha

---
+++ App_Clima/src/DAO/ConecSQL.java
@@ -0,0 +1,23 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class ConecSQL {
+    public Connection conectaBD () {
+        Connection conn = null;
+        
+        try {
+            String url = "jdbc:mysql://localhost:3306/Database_Users?user=root&password=arcoeflecha";
+            conn = DriverManager.getConnection(url); 
+            
+        } catch (SQLException error) {
+            JOptionPane.showMessageDialog(null, "ConecSQL: " + error);
+        }
+        
+        return conn;
+        
+    }
+}
