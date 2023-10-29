bd9e99af899395c5687cf3b54a5ac09d97c18d7b
marshfellow42 <leandrocsousa9@gmail.com>
2023-10-29 00:47:06 -0300

    Adicionar um lugar para colocar todo o funcionamento da API
    Também vem incluida o ID da nossa API, já que eu não tenho saco para deixar isso escondido no GitHub e ter toda a dor de cabeça para organizar isso depois

---
+++ App_Clima/src/DAO/ConecAPI.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+/**
+ *
+ * @author Elenita
+ */
+public class ConecAPI {
+    // http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid=39b4b8957534286d3d7a4a23f5bd3ee6
+}
