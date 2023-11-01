80b38535ffaf0a485c94ecb280f3550271790de2
Leandro Cândido <123888466+marshfellow42@users.noreply.github.com>
2023-10-31 22:14:49 -0300

    Removing a name

--- App_Clima/src/DAO/ConecAPI.java
+++ App_Clima/src/DAO/ConecAPI.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package DAO;
 
-/**
- *
- * @author Elenita
- */
 public class ConecAPI {
     // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=39b4b8957534286d3d7a4a23f5bd3ee6
 }
